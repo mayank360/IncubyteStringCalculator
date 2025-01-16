74b7bcccdcf3c6488154f9134c7b270c9fb78111
guptam11_cat <mayank.gupta2@cat.com>
2025-01-16 19:33:17 +0530

    Initial commit - creating test class and handling empty string case

---
+++ src/StringCalculator/stringCalculatorTest.java
@@ -0,0 +1,15 @@
+package StringCalculator;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class stringCalculatorTest {
+    @Test
+    public void addTest(){
+        Assert.assertEquals(add(""),0);
+    }
+
+    private int add(String s) {
+        return 0;
+    }
+}
