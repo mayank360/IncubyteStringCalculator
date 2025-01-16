e9d503caaa35937a70674e2ece6117de0a2bfab9
guptam11_cat <mayank.gupta2@cat.com>
2025-01-16 22:45:50 +0530

    fourth commit - handling string containing next line character with code refactoring

--- src/StringCalculator/stringCalculatorTest.java
+++ src/StringCalculator/stringCalculatorTest.java
@@ -13,15 +13,18 @@ public class stringCalculatorTest {
         Assertions.assertEquals(add("5"), 5);
         Assertions.assertEquals(add("1,2"), 3);
         Assertions.assertEquals(add("1,2,4,3"), 10);
+        Assertions.assertEquals(add("1\n2,3,4"), 10);
+        Assertions.assertEquals(add("1\n2,3\n4"), 10);
     }
 
     private int add(String s) {
         int sum = 0;
-        if(s.isEmpty())
-            return 0;
-        String[] numbers = s.split(",");
-        for(String num:numbers)
-            sum += Integer.parseInt(num);
+        if(!s.isEmpty()) {
+            s = s.replace("\n", ",");
+            String[] numbers = s.split(",");
+            for (String num : numbers)
+                sum += Integer.parseInt(num);
+        }
         return sum;
     }
 }
