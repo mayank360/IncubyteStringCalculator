00418c7d4a88e9b07123bde5eefcd2163386d5ad
guptam11_cat <mayank.gupta2@cat.com>
2025-01-16 22:07:36 +0530

    Second commit - handling string containing one integer

--- src/StringCalculator/stringCalculatorTest.java
+++ src/StringCalculator/stringCalculatorTest.java
@@ -1,15 +1,21 @@
 package StringCalculator;
 
-import org.junit.Assert;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class stringCalculatorTest {
     @Test
     public void addTest(){
-        Assert.assertEquals(add(""),0);
+
+        Assertions.assertEquals(add(""), 0);
+        Assertions.assertEquals(add("1"), 1);
+        Assertions.assertEquals(add("5"), 5);
     }
 
     private int add(String s) {
-        return 0;
+        if(s.isEmpty())
+            return 0;
+        return Integer.parseInt(s);
     }
 }
